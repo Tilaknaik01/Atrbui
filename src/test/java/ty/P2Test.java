1e58624c77134d79856e87da95b674172979fdd0
Tilaknaik01 <tilaknaik@yagoo.com>
2023-06-04 16:56:45 +0530

    commit4wdd

--- src/test/java/ty/P2Test.java
+++ src/test/java/ty/P2Test.java
@@ -2,7 +2,7 @@ package ty;
 
 
 
-public class P2 {
+public class P2Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/test/java/ty/P3Test.java
+++ src/test/java/ty/P3Test.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class Pgm1
+public class P3Test
 {
 	public static void  main(String[] args)
 	{
